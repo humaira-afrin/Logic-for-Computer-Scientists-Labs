d77cd1009a14e8022837f850b7ef6ee96dcab10f
Humaira Afrin <humairaa@kth.se>
2024-09-26 12:03:11 +0200

    Update Ex-4.pl

--- LAB-1/Ex-4.pl
+++ LAB-1/Ex-4.pl
@@ -1,18 +1,12 @@
-granne1(A):-granne(A,X),
-granne(X,A).
-
-granne1(b,c).
-granne(c,b).
-
+granne(a,b).
+granne(b,c).
 granne(c,d).
-granne(d,c).
-
-
 granne(d,e).
-granne(e,d).
-
 granne(e,f).
 granne(f,a).
 granne(c,g).
 granne(g,i).
 
+
+granne1(A):-(granne(A,X);
+granne(X,A)), write(X).
