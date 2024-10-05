fbd32b9fffdc7d631eee3908843883eb0991ed87
Humaira Afrin <humairaa@kth.se>
2024-10-05 21:45:04 +0200

    Update Ex-4.pl

--- LAB-1/Ex-4.pl
+++ LAB-1/Ex-4.pl
@@ -9,6 +9,8 @@ granne(g,i).
 
 granne1(A,X):- granne(A,X); granne(X,A).
 
-path(A, A, _, [A]).
-path(A, B, Acc, [A|T]) :- granne1(A, X),\+ memberchk(X, Acc),         
-    path(X, B, [X|Acc], T).     
+path1(A, A, _, [A]).
+path1(A, B, Acc, [A|T]) :- granne1(A, X),\+ memberchk(X, Acc),         
+    path1(X, B, [X|Acc], T).     
+
+path(A,B,T):- path1(A,B,[A],T).
