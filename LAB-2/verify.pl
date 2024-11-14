ffa46ee4a39c5b36bfbc83e5582d7b6810fa9a36
Humaira Afrin <humairaa@kth.se>
2024-11-14 13:31:46 +0100

    Update verify.pl

--- LAB-2/verify.pl
+++ LAB-2/verify.pl
@@ -1,15 +1,33 @@
-% Från instruktion
+:- discontiguous rule/4.
+
 verify(InputFileName) :- see(InputFileName),
     read(Prems), read(Goal), read(Proof),
     seen,
     valid_proof(Prems, Goal, Proof).
+
+valid_proof(Prems, Goal, Proof) :- 
+    helper(Prems, Goal, Proof, []).
+
+helper(Prems, Goal, [[Num, Step, Rule] | Rest], Bevisat) :-
+        rule(Rule, Prems, Bevisat, Step),
+        helper(Prems, Goal, Rest, [[Num, Step, Rule] | Bevisat]).
+    
+helper(Prems, Goal, [[_, Goal, Rule]], Bevisat):-  rule(Rule, Prems, Bevisat, Goal). 
+    
+%  Assumption
+helper(Prems, Goal, [Del|RestSteps], Bevisat)
+   :- Del = [FirstLine|RestOfSection],
+      FirstLine = [_, _, assumption],
+      (RestOfSection = [] ; helper(Prems, _, RestOfSection, [FirstLine|Bevisat])),
+     helper(Prems, Goal, RestSteps, [Del|Bevisat]).
+
 
 
 % R E G L E R
 % X och Y är step numbers och predikate följer tsrukturen 
 % rule(Regel, Premises, Bevisat, conclusion)
 
-%  Premise
+% Premise
 rule(premise, Premis, _, A) :- member(A, Premis).
 
 % ∧i 
@@ -30,15 +48,17 @@ rule(negel(X, Y), _, Bevisat, motsagelse) :-
     member([X, A, _], Bevisat),
     member([Y, neg(A), _], Bevisat).
 
-% Box kollar om Bevisat har en en som börjar med X och slutar med Y
-box(X,Y,Bevisat):- member(Z,Bevisat),
-     append([X],_,Z), append(_,[Y],Z).
+% Box checks if Bevisat has a proof that starts with X and ends with Y
+box(Start, End, Bevisat) :- 
+    member(Z, Bevisat),
+    append([Start], _, Z), 
+    append(_, [End], Z).
 
 % ¬i (A (assumption),....⊥ |- ¬A)
 rule(negint(X, Y), _, Bevisat, neg(A)) :- 
     box([X, A, assumption], [Y, motsagelse, _], Bevisat).
 
-% ⊥e (⊥ då kan man skriva vadsomhelst)
+% ⊥e (⊥ allows anything)
 rule(motsagelseel(X), _, Bevisat, _) :- 
     member([X, motsagelse, _], Bevisat).
 
@@ -54,12 +74,12 @@ rule(negnegel(X), _, Bevisat, A) :-
 rule(copy(X), _, Bevisat, A) :- 
     member([X, A, _], Bevisat).
 
-% MT (A-->B, icke B |- icke A)
+% MT (A-->B, not B |- not A)
 rule(mt(X, Y), _, Bevisat, neg(A)) :- 
     member([X, imp(A, B), _], Bevisat),
     member([Y, neg(B), _], Bevisat).
 
-% PBC (Assume icke A.....⊥ |- A)
+% PBC (Assume not A....⊥ |- A)
 rule(pbc(X, Y), _, Bevisat, A) :- 
     box([X, neg(A), assumption], [Y, motsagelse, _], Bevisat).
 
@@ -83,7 +103,7 @@ rule(orint1(X), _, Bevisat, or(A, _)) :-
 rule(orint2(X), _, Bevisat, or(_, A)) :- 
     member([X, A, _], Bevisat).
 
-% ∨e assumption B leder till A och assumption C leder till A 
+% ∨e assumption B leads to A and assumption C leads to A
 rule(orel(I, II, III, IV, V), _, Bevisat, A) :- 
     member([I, or(B, C), _], Bevisat),
     box([II, B, assumption], [III, A, _], Bevisat),
