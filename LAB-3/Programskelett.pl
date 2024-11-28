c244a1305f32d2e418fdf7150c1350015dd58661
Humaira Afrin <humaira.afrin.stockholm@gmail.com>
2024-11-29 00:14:30 +0100

    CTL rules, not done

--- LAB-3/Programskelett.pl
+++ LAB-3/Programskelett.pl
@@ -18,13 +18,17 @@ check(T, L, S, [], F).
 % U
 % To execute: consult('your_file.pl'). verify('input.txt').
 % Literals
+
+% kollar om X är en label [s,x] i current state S
 %check(_, L, S, [], X) :- 
   member([S, Propositions], L), 
   member(X, Propositions).
-%Neg
+
+% Neg X is false in D so neg(X) is true
 %check(_, L, S, [], neg(X)) :-
   member([S, Propositions], L), 
   \+ member(X, Propositions). 
+
 % And
 %check(T, L, S, [], and(F,G)) :- 
   check(T, L, S, [], F),
@@ -37,10 +41,51 @@ check(T, L, S, [], F).
 
 %Temporala operatorer
 % AX
-%check(T, L, S, [], ax(F)):- 
+%check(T, L, S, [], ax(F)):- check(T, L, S, U, F),
+member([S,Next],T),
+next_all_path(T,L,Next,[],F).
+
 % EX
-%check(T, L, S, [], ex(F)):- 
-% AG
-% EG
-% EF
-% AF
+%check(T, L, S, [], ex(F)):- check (T,L,S,[],F);
+  member([S,Next], T),
+  next_some_path(T,L,Next,[],F).
+
+% AG1 
+check(T,L,S,U,ag(F)):- member(S,U).
+% AG 2
+check(T,L,S,U,ag(F)):- \+member(S,U),
+      check(T,L,S,[],F), member([S,Next],T),
+      append([S],U,Recorded),
+      next_all_path(T,L,Next,Recorded,ag(F)).
+
+% EG1
+check(T,L,S,U,eg(F)):- member(S,U).
+%EG 2 Ngn path Next som har alla F
+check(T,L,S,U,eg(F)):- \+member(S,U),
+      check(T,L,S,[],F),
+      member([S,Next],T),
+      next_some_path(T,L,Next,[S|U],eg(F)).
+
+
+% EF 1
+check(T,L,S,U,ef(F)):- \member+(S,U),
+      check(T,L,S,[],F).
+% EF 1
+check(T,L,S,U,ef(F)):-\member+(S,U), member([S,Next],T),
+      append([S],U,Recorded).
+      next_some_path(T,L,Next,Recorded,eg(F)).
+
+% AF 1
+check(T,L,S,U,af(F)):- \member+(S,U),
+      check(T,L,S,[],F).
+% AF 2
+check(T,L,S,U,af(F)):-
+% U is empty
+next_all_path (T,L,[H|R],[],F):-check(T,L,H,[],F),
+            check_states(T,L,R,[],F).
+% U not empty
+next_all_path(T,L,[H|R],U,F):-check(T,L,H,U,F),
+            check_states(T,L,R,[H|U],F).
+
+next_some_path (T,L,[H|R],[],F):-check(T,L,H,U,F);
+        next_some_path(T,L,R,[],F).
